91850bcafea0b8b57176b8de9c30ba3c9b1ea2fd
Ben Halim <bhalim1994@gmail.com>
2022-08-29 08:49:52 -0700

    Added error checking for title, release_year, duration_minutes, and rating

--- api/views.py
+++ api/views.py
@@ -15,6 +15,16 @@ def post_movie():
     duration_minutes = request.get_json().get('duration_minutes')
     rating = request.get_json().get('rating')
 
+    if Movie.query.filter_by(title=title).first():
+        return jsonify({'Error': 'Movie already exists in the database'}), 409
+
+    if is_invalid_release_year(release_year):
+        return jsonify({'Error': 'release_year needs to be a positive 4-integer value'}), 409
+    if is_invalid_duration_minutes(duration_minutes):
+        return jsonify({'Error': 'duration_minutes needs to be an integer greater than 0'}), 409
+    if is_invalid_rating(rating):
+        return jsonify({'Error': 'rating needs to be an integer'}), 409
+
     movie = Movie(title=title,
                   description=description,
                   release_year=release_year,
@@ -61,11 +71,26 @@ Fully updates a movie using its ID
 def update_movie(movie_id):
     movie = Movie.query.filter_by(id=movie_id).first()
 
+    if not movie:
+        return jsonify({'Error': 'Movie not found'}), 404
+
     title = request.get_json().get('title')
     description = request.get_json().get('description')
     release_year = request.get_json().get('release_year')
     duration_minutes = request.get_json().get('duration_minutes')
     rating = request.get_json().get('rating')
+
+    if is_invalid_release_year(release_year):
+        return jsonify({'Error': 'release_year needs to be a positive 4-integer value'}), 409
+    if is_invalid_duration_minutes(duration_minutes):
+        return jsonify({'Error': 'duration_minutes needs to be an integer greater than 0'}), 409
+    if is_invalid_rating(rating):
+        return jsonify({'Error': 'rating needs to be an integer'}), 409
+
+    if description is None:
+        description = "No description provided"
+    if rating is None:
+        rating = 0
 
     movie.title = title
     movie.description = description
@@ -92,6 +117,9 @@ Partially updates a movie using its ID
 def partial_update_movie(movie_id):
     movie = Movie.query.filter_by(id=movie_id).first()
 
+    if not movie:
+        return jsonify({'Error': 'Movie not found'}), 404
+
     title = request.get_json().get('title')
     description = request.get_json().get('description')
     release_year = request.get_json().get('release_year')
@@ -102,11 +130,21 @@ def partial_update_movie(movie_id):
         movie.title = title
     if description:
         movie.description = description
-    if release_year:
+
+    if is_invalid_release_year(release_year):
+        return jsonify({'Error': 'release_year needs to be a positive 4-integer value'}), 409
+    elif release_year:
         movie.release_year = release_year
-    if duration_minutes:
+
+    if is_invalid_duration_minutes(duration_minutes):
+        return jsonify({'Error': 'duration_minutes needs to be an integer greater than 0'}), 409
+    elif duration_minutes:
         movie.duration_minutes = duration_minutes
-    if rating:
+
+    if is_invalid_rating(rating):
+        return jsonify({'Error': 'rating needs to be an integer'}), 409
+    # Cannot do "elif rating:" as a rating of 0 would evaluate to False and discard a rating of 0
+    elif isinstance(rating, int):
         movie.rating = rating
 
     db.session.commit()
@@ -205,3 +243,31 @@ def dislike_movie(movie_id):
         'duration_minutes': movie.duration_minutes,
         'rating': movie.rating
     }), 200
+
+
+'''
+True if release_year's format is invalid, false otherwise
+'''
+def is_invalid_release_year(release_year):
+    # Check if release_year is not an int (None OK since db handles nullables)
+    # If release_year is an int, ensure it's in bounds
+    return (not isinstance(release_year, int) and release_year is not None) or \
+           (isinstance(release_year, int) and (release_year < 1000 or release_year > 9999))
+
+
+'''
+True if duration_minutes' format is invalid, false otherwise
+'''
+def is_invalid_duration_minutes(duration_minutes):
+    # Check if duration_minutes is not an int (None OK since db handles nullables)
+    # If duration_minutes is an int, ensure it's positive
+    return (not isinstance(duration_minutes, int) and duration_minutes is not None) or \
+           (isinstance(duration_minutes, int) and (duration_minutes <= 0))
+
+
+'''
+True if rating's format is invalid, false otherwise
+'''
+def is_invalid_rating(rating):
+    # Check if rating is not an int (None OK since db handles nullables)
+    return not isinstance(rating, int) and rating is not None
